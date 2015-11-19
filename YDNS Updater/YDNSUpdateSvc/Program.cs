392adf46031e28bc799985a30578c5813fdafb71
DARKGuy <darkguy2008@gmail.com>
2015-11-19 02:20:09 -0430

    Service ready. Needs GUI now. Also, needs sample .ini file to be uploaded.

--- YDNS Updater/YDNSUpdateSvc/Program.cs
+++ YDNS Updater/YDNSUpdateSvc/Program.cs
@@ -1,15 +1,46 @@
-﻿using System;
+﻿using Qube.ConsoleApp;
+using Qube.IO;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace YDNSUpdateSvc
 {
-    class Program
+    class Program : ConsoleApplication
     {
-        static void Main(string[] args)
+        public static Dictionary<string, Dictionary<string, string>> Config;
+
+        public static void Main(string[] args)
         {
+            var app = new Program();
+            app.Run(args);
+        }
+
+        public override void Run(string[] args)
+        {
+            base.Run(args);
+            Config = INIFile.Read(AppPath + "Config.ini");
+
+            WebClient wc = new WebClient();
+            if (Config["Proxy"]["User"].Length > 0)
+            {
+                wc.UseDefaultCredentials = false;
+                wc.Proxy.Credentials = new NetworkCredential(Config["Proxy"]["User"], Config["Proxy"]["Pass"], Config["Proxy"]["Domain"]);
+            }
+
+            string myIP = wc.DownloadString("http://myexternalip.com/raw");
+
+            string auth = Config["API"]["User"] + ":" + Config["API"]["Key"];
+            auth = Convert.ToBase64String(Encoding.ASCII.GetBytes(auth));
+            wc.Headers.Add(HttpRequestHeader.Authorization, "Basic " + auth);
+
+            foreach (string host in Config["Hosts"].Keys)
+                Console.WriteLine(wc.DownloadString(string.Format("https://ydns.io/api/v1/update/?host={0}&ip={1}", host, myIP)));
+
+            Console.ReadKey();
         }
     }
 }
